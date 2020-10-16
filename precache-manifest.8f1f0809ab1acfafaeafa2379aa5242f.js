ae067f646d2238b1907614681ad23b1899787e07
SWH <seok97>
2020-10-16 22:58:20 +0900

    Updates

---
+++ precache-manifest.8f1f0809ab1acfafaeafa2379aa5242f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b983dd7fcd3577482ac2e9b9d571f00",
+    "url": "/movielist2020/index.html"
+  },
+  {
+    "revision": "570cad06e3ee4a5d5657",
+    "url": "/movielist2020/static/css/main.1a58af07.chunk.css"
+  },
+  {
+    "revision": "8c9b3f1f87628c593884",
+    "url": "/movielist2020/static/js/2.21f8caf1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movielist2020/static/js/2.21f8caf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570cad06e3ee4a5d5657",
+    "url": "/movielist2020/static/js/main.5dee91a5.chunk.js"
+  },
+  {
+    "revision": "12bde63e69ba92680e9f",
+    "url": "/movielist2020/static/js/runtime-main.f7601d59.js"
+  }
+]);
